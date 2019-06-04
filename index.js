043b9c3a44b0e9c510e13fd2b4666d2aa138fb30
jdc996 <jdcubillos96@gmail.com>
2019-06-04 16:59:53 -0500

    Se implementa login de flujo oidc #2

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+import {hash} from './md5.js';
 
-import { Md5 } from 'ts-md5/dist/md5';
-import { Observable } from 'rxjs/Rx';
+var GENERAL = 
+{
+  ENTORNO:{
+    TOKEN:{}
+  }
+}
 
 if (window.localStorage.getItem('access_token') === null ||
   window.localStorage.getItem('access_token') === undefined) {
-  const params = {},
+  var params = {},
     queryString = location.hash.substring(1),
-    regex = /([^&=]+)=([^&]*)/g;
+    regex = /([^&=]+)=([^&]*)/g;  
   let m;
   while (m = regex.exec(queryString)) {
 
@@ -19,6 +24,7 @@ if (window.localStorage.getItem('access_token') === null ||
   req.open('GET', query, true);
   if (params['id_token'] !== null && params['id_token'] !== undefined) {
     window.localStorage.setItem('access_token', params['access_token']);
+    //if token setearñp
     window.localStorage.setItem('id_token', params['id_token']);
     window.localStorage.setItem('expires_in', params['expires_in']);
     window.localStorage.setItem('state', params['state']);
@@ -37,7 +43,7 @@ if (window.localStorage.getItem('access_token') === null ||
       }
     }
   };
-} else {
+  } /*else {
   let state;
   const queryString = location.search.substring(1);
   const regex = /([^&=]+)=([^&]*)/g;
@@ -53,38 +59,44 @@ if (window.localStorage.getItem('access_token') === null ||
       window.history.replaceState({}, document.title, clean_uri);
     }
   }
-}
+}*/
 
 export function init() {
-  this.clearUrl();
-  this.logOut = '';
-  this.timer();
-}
-const GENERAL = {
-  ENTORNO: {
-    TOKEN: {
-      AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
-      URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
-      CLIENTE_ID: "pszmROXqfec4pTShgF_fn2DAAX0a",
-      REDIRECT_URL: "http://localhost:9000/",
-      RESPONSE_TYPE: "id_token token",
-      SCOPE: "openid email",
-      BUTTON_CLASS: "btn btn-warning btn-sm",
-      SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
-      SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
-      SIGN_OUT_APPEND_TOKEN: "true",
-      REFRESH_TOKEN: "https://autenticacion.udistrital.edu.co/oauth2/token",
-    }
-  }
+  clearUrl();
+  //var logOut = '';
+  timer();
 }
 
+export function setGeneral(url_token){
+  GENERAL.ENTORNO.TOKEN=url_token;
+}
+
+// const GENERAL = {
+//   ENTORNO: {
+//     TOKEN: {
+//       AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
+//       URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
+//       CLIENTE_ID: "pszmROXqfec4pTShgF_fn2DAAX0a",
+//       REDIRECT_URL: "http://localhost:9000/",
+//       RESPONSE_TYPE: "id_token token",
+//       SCOPE: "openid email",
+//       BUTTON_CLASS: "btn btn-warning btn-sm",
+//       SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
+//       SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
+//       SIGN_OUT_APPEND_TOKEN: "true",
+//       REFRESH_TOKEN: "https://autenticacion.udistrital.edu.co/oauth2/token",
+//     }
+//   }
+// }
+
+
 export function logout() {
-  this.logOut = GENERAL.ENTORNO.TOKEN.SIGN_OUT_URL;
-  this.logOut += '?id_token_hint=' + window.localStorage.getItem('id_token');
-  this.logOut += '&post_logout_redirect_uri=' + GENERAL.ENTORNO.TOKEN.SIGN_OUT_REDIRECT_URL; // // + window.location.href; para redirect con regex
-  this.logOut += '&state=' + window.localStorage.getItem('state');
-  window.location.replace(this.logOut);
-  return this.logOut;
+  let logOut = GENERAL.ENTORNO.TOKEN.SIGN_OUT_URL;
+  logOut += '?id_token_hint=' + window.localStorage.getItem('id_token');
+  logOut += '&post_logout_redirect_uri=' + GENERAL.ENTORNO.TOKEN.SIGN_OUT_REDIRECT_URL; // // + window.location.href; para redirect con regex
+  logOut += '&state=' + window.localStorage.getItem('state');
+  window.location.replace(logOut);
+  
 }
 
 export function clearUrl() {
@@ -116,36 +128,39 @@ export function live() {
     this.setExpiresAt();
     return true;
   } else {
+    this.getAuthorizationUrl()
     return false;
   }
 }
 
 export function getAuthorizationUrl() {
-  this.params = GENERAL.ENTORNO.TOKEN;
-  if (!this.params.nonce) {
-    this.params.nonce = this.generateState();
+   params = GENERAL.ENTORNO.TOKEN;
+  if (!params.nonce) {
+    params.nonce = this.generateState();
   }
-  if (!this.params.state) {
-    this.params.state = this.generateState();
+  if (!params.state) {
+    params.state = this.generateState();
   }
-  let url = this.params.AUTORIZATION_URL + '?' +
-    'client_id=' + encodeURIComponent(this.params.CLIENTE_ID) + '&' +
-    'redirect_uri=' + encodeURIComponent(this.params.REDIRECT_URL) + '&' + // + window.location.href + '&' para redirect con regex
-    'response_type=' + encodeURIComponent(this.params.RESPONSE_TYPE) + '&' +
-    'scope=' + encodeURIComponent(this.params.SCOPE) + '&' +
+  let url = params.AUTORIZATION_URL + '?' +
+    'client_id=' + encodeURIComponent(params.CLIENTE_ID) + '&' +
+    'redirect_uri=' + encodeURIComponent(params.REDIRECT_URL) + '&' + // + window.location.href + '&' para redirect con regex
+    'response_type=' + encodeURIComponent(params.RESPONSE_TYPE) + '&' +
+    'scope=' + encodeURIComponent(params.SCOPE) + '&' +
     'state_url=' + encodeURIComponent(window.location.hash);
-  if (this.params.nonce) {
-    url += '&nonce=' + encodeURIComponent(this.params.nonce);
+  if (params.nonce) {
+    url += '&nonce=' + encodeURIComponent(params.nonce);
   }
-  url += '&state=' + encodeURIComponent(this.params.state);
+  url += '&state=' + encodeURIComponent(params.state);
   // alert(url);
+  window.location = url;
   return url;
 }
 
 
 export function generateState() {
   const text = ((Date.now() + Math.random()) * Math.random()).toString().replace('.', '');
-  return Md5.hashStr(text);
+  console.log("general: "+GENERAL.ENTORNO.TOKEN.AUTORIZATION_URL)
+  return hash(text);
 }
 
 export function setExpiresAt() {
@@ -162,11 +177,11 @@ export function expired() {
 }
 
 export function timer() {
-  Observable.interval(5000).subscribe(() => {
+  setInterval(()=>{
     if (window.localStorage.getItem('expires_at') !== null) {
       if (this.expired()) {
         window.localStorage.clear();
       }
     }
-  });
+  },5000)
 }
